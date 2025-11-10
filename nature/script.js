7fb344c840561410e74be072760645b2bdb32911
sshafiq1 <183101652+sshafiq1@users.noreply.github.com>
2025-11-10 14:03:25 -0500

    in class exercise

---
+++ nature/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".purpleleaf").hover(function(){
+        $(".yellowleaf").fadeIn(3000);
+        $("h1").fadeIn(1000);
+        $(".greenleaf").fadeIn(2000);
+    });
+});
