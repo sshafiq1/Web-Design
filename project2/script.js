487432d0ac5025601fe76e2f9230796b00d0214a
sshafiq1 <183101652+sshafiq1@users.noreply.github.com>
2025-11-24 10:39:43 -0500

    project 2

--- project2/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 100) {
-        document.body.classList.add('background-color: #344338;');
-    } else {
-        document.body.classList.remove('background-color: #f6efe0;');
-    }
-});
