9348d3ecec40cfc826a68752bcc2c05607124edb
sshafiq1 <183101652+sshafiq1@users.noreply.github.com>
2025-12-01 13:47:46 -0500

    nature/jquery exercise

--- nature/script.js
+++ nature/script.js
@@ -3,5 +3,29 @@ $(document).ready(function(){
         $(".yellowleaf").fadeIn(3000);
         $("h1").fadeIn(1000);
         $(".greenleaf").fadeIn(2000);
+    }, function(){
+        $(".yellowleaf").fadeOut("fast");
+        $(".greenleaf").fadeOut("fast");
+    });
+
+    $(".rose").hover(function(){
+        $(".rosetext").fadeToggle();
+        $(".daisy").fadeToggle();
+        $(".rose2").fadeToggle();
+        $(".rose3").fadeToggle();
+    });
+
+    $(".daisy").hover(function(){
+        $(".daisytext").fadeToggle();
+        $(".rose").fadeToggle();
+        $(".daisy2").fadeToggle();
+    });
+
+    $(".burger").click(function(){
+        $(".nav").fadeToggle();
+    });
+
+    $("a").click(function(){
+        $(".nav").fadeToggle(); // alt use hide for it to disappear immediatly
     });
 });
